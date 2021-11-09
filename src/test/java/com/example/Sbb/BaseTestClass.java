dd6be51c757511af02a5e4b66c0e23e81c1d1d0b
Vladimir <trofimowova@yandex.ru>
2021-11-09 15:03:42 +0300

    Added BaseClassTest (with unknown Controller), configuration of baseclass

---
+++ src/test/java/com/example/Sbb/BaseTestClass.java
@@ -0,0 +1,13 @@
+package com.example.Sbb;
+
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+
+public class BaseTestClass {
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(new FraudController());
+    }
+}
