10854710c414b6abc590046bb7610b053b805255
Vladimir <trofimowova@yandex.ru>
2021-11-09 17:24:17 +0300

    Project before implementing Datarepo

--- src/main/java/com/example/Sbb/DataController.java
+++ src/main/java/com/example/Sbb/DataController.java
@@ -9,29 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DataController {
 
-    @GetMapping(value = "/datagroups", consumes = "application/json", produces = "application/json")
-    public String check(@RequestBody DataClass dataClass) {
-
-        if (dataClass.getId() == 154101) {
-            return "{id: 154101, beschreibung:DATAGROUP_117.2.20191206-2201.P.P.22331107.0_MIN}";
-        }
-
-        return null;
+    @GetMapping(value = "/datagroups")
+    public String showDataGroup(){
+        return " STUB";
     }
+
+
+
 }
 
 
 
 
- /* {"id ":"154101",
-                "beschreibung":"DATAGROUP_117.2.20191206-2201.P.P.22331107.0_MIN",
-                "adcDump":null,
-                "readyForSmoke":false,
-                "url":anyAlphaUnicode(),
-                "erstellungsZeitpunkt":anyAlphaUnicode(),
-                "status":anyAlphaUnicode(),
-                "postProcessorVersion":null,
-                "exporterVersion":null,
-                "adcTemporal":false,
-                "adcBuildNumber":null,
-                "latestPlandatenId":null};*/
+
--- src/test/resources/contracts/ContractExample.groovy
+++ src/test/resources/contracts/ContractExample.groovy
@@ -15,9 +15,19 @@ org.springframework.cloud.contract.spec.Contract.make {
     response {
         status OK()
         body(
-
-               [ id:154101,
-                "beschreibung":"DATAGROUP_117.2.20191206-2201.P.P.22331107.0_MIN"]
+               [ "id":154101,
+                "beschreibung":"DATAGROUP_117.2.20191206-2201.P.P.22331107.0_MIN",
+                 "beschreibung":"DATAGROUP_117.2.20191206-2201.P.P.22331107.0_MIN",
+                 "adcDump":null,
+                 "readyForSmoke":false,
+                 "url":anyAlphaUnicode(),
+                 "erstellungsZeitpunkt":anyAlphaUnicode(),
+                 "status":anyAlphaUnicode(),
+                 "postProcessorVersion":null,
+                 "exporterVersion":null,
+                 "adcTemporal":false,
+                 "adcBuildNumber":null,
+                 "latestPlandatenId":null]
 
         )
         headers {
@@ -27,15 +37,3 @@ org.springframework.cloud.contract.spec.Contract.make {
 }
 
 
-/* "id":"154101",
-                "beschreibung":"DATAGROUP_117.2.20191206-2201.P.P.22331107.0_MIN",
-                "adcDump":null,
-                "readyForSmoke":false,
-                "url":anyAlphaUnicode(),
-                "erstellungsZeitpunkt":anyAlphaUnicode(),
-                "status":anyAlphaUnicode(),
-                "postProcessorVersion":null,
-                "exporterVersion":null,
-                "adcTemporal":false,
-                "adcBuildNumber":null,
-                "latestPlandatenId":null]*/
